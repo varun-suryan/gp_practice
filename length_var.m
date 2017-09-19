cb722bd7f5548ba9a079761f1fb8c361158721f9
varun <varunsuryan9@gmail.com>
2017-09-19 01:12:56 -0400

    2-d

--- length_var.m
+++ length_var.m
@@ -3,22 +3,28 @@ clc;
 close all;
 
 
-points = [1 : 0.5 : 100]';
+[pointsX, pointsY] =  meshgrid([1 : 0.1 : 5], [1 : 0.1 : 5]);
 
-l = 10/sqrt(2);
+l = 0.5/sqrt(2);
 
 
 signal_std = 1;
 
-noise = 0.0;
+noise = 0;
 
-kernel = signal_std^2 * exp (- squareform(pdist(points)).^2 / (2 * l^2) ) + noise^2 * eye(numel(points));
-
-R = mvnrnd( zeros(numel(points), 1) , kernel, 1);
+kernel = signal_std^2 * exp (- squareform(pdist([pointsX(:), pointsY(:)])).^2 / (2 * l^2) ) + noise^2 * eye(numel(pointsX));
 
 
-plot(points, R);
-hold on;
+R = mvnrnd( zeros(numel(pointsX), 1) , kernel, 1);
+
+R = reshape(R, size(pointsX));
+
+surf(R)
+
+% plot(points, R);
+
+
+% hold on;
 % axis([1 100 -20 20]);
 
 
