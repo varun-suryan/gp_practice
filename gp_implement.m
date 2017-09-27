78a4a0357845b47e41e98f98ee3609d3d28e8bff
varun <varunsuryan9@gmail.com>
2017-09-27 15:08:02 -0400

    sequential implementation for 1-D case done

--- gp_implement.m
+++ gp_implement.m
@@ -1,68 +1,88 @@
 clear all;
 clc;
 close all;
-GRID = 1;
-% Create the state action space in 2-D
-[stateX, stateY] = meshgrid(-GRID: 1 :GRID, -GRID: 1 :GRID);
-actions = [0 1; 1 0; 0 -1; -1 0]; 
-state_action_space = [repmat([stateX(:) stateY(:)], size(actions, 1), 1) repmat(actions, numel(stateX), 1)];
+GRID = 5;
+epsilon = 0.05;
+gamma_ =0.9;
+alpha_ = 1.0;
+% Create the state action space in 1-D
+states = [-GRID : 1 : GRID]';
+actions = [-1; 1]; 
+
+design_matrix = [repmat(states, numel(actions), 1) repmat(actions, numel(states), 1) zeros(numel(states) * numel(actions), 1)];
 
 
 % Append initial Q-values with state action space
-design_matrix = [state_action_space zeros(size(state_action_space(:, 1)))];
+% design_matrix = [state_action_space zeros(size(state_action_space(:, 1)))];
 
 
 % surf(stateX, stateY, stateX.^2 + stateY.^2);
 
-l = 6/sqrt(2);
+l = 1/sqrt(2);
 
-signal_std = 2;
+signal_std = 5;
 noise = 0.1;
 
-kernel_ = signal_std^2 * exp (- squareform(pdist(state_action_space)).^2 / (2 * l^2) ) + noise^2 * eye(size(state_action_space,1));
+kernel_ = signal_std^2 * exp (- squareform(pdist(design_matrix(:, end - 1))).^2 / (2 * l^2) ) + noise^2 * eye(size(design_matrix,1));
 
-mean_ = zeros(size(state_action_space,1), 1);
-R_prior = mvnrnd(mean_ , kernel_, 1);
+mean_ = zeros(size(design_matrix, 1), 1);
+% R_prior = mvnrnd(mean_, kernel_, 1);
 
-pause();
 
-plot(points, R_prior,'r');
-hold on;
+% plot(points, R_prior,'r');
+
+% design_matrix(:, 1 : end - 1)
+
+% Plot the Q-Function
+% surf(reshape(design_matrix(:, 1), numel(states), 2), reshape(design_matrix(:, 2), numel(states), 2), reshape(R_prior, numel(states), 2));
+% hold on;
+
+
 % axis([1 100 -20 20]);
 
 
-frequency = 40;
-points_train = points(1 : frequency : end);
-points_train_target = sin(points_train) + 0.05 * randn(size(points_train));
+% frequency = 40;
+% points_train = points(1 : frequency : end);
+% points_train_target = sin(points_train) + 0.05 * randn(size(points_train));
 
 
+curr_state = 2;
 
+while curr_state ~= GRID	
+	if rand <= epsilon
+		action = datasample([-1 1], 1);
+	else
+		temp = design_matrix(find(design_matrix(:, 1) == curr_state), :);
+		[M, I] = max(temp(:, 3));
+		action = temp(I, 2);
+	end
 
+	% next_state = (max(min(curr_state(1) + action(1), GRID), -GRID) , max(min(curr_state(2) + action(2), GRID), -GRID));
+	next_state = max(min(curr_state + action, GRID), -GRID);
 
-% for time_step = 1 : 100
+	[~, indexing, ~] = intersect(design_matrix(:, 1 : end -1), [curr_state action], 'rows');
+
+	observation = (1 - alpha_) * design_matrix(indexing, end) + alpha_ * reward_dynamic(next_state, GRID) + alpha_ * gamma_ * max(design_matrix(find(design_matrix(:, 1) == next_state), end));
 	
-% 	action = chooseAction(curr_state)
+	coeff = 1 / (kernel_(indexing, indexing) + noise^2 * eye(numel(indexing)));
 
-	
-% 	next_state = (max(min(curr_state(1) + action(1), GRID), -GRID) , max(min(curr_state(2) + action(2), GRID), -GRID))
-	
-% 	design_mat_t.append(reward_dynamics(next_state) + gamma * max([getQ(next_state, a) for a in actions]))
-	
-
-% 	y_pred, sigma = gp.predict(test, return_std = True)
-
-% 	curr_state = next_state
-% end
+	mean_ = mean_ + kernel_(:, indexing) * coeff * (observation - mean_(indexing));	
+	kernel_ = kernel_ - kernel_(:, indexing) * coeff * kernel_(indexing, :) + noise^2;
+	design_matrix(:, end) = mean_ + 1.96 * diag(kernel_);
+	% y_pred, sigma = gp.predict(test, return_std = True)
+	curr_state = next_state
+	pause(1);
+	design_matrix
+end
 
 
 % Sequential Implementation
-count = 1;
-for indexing = 1 : frequency : size(points, 1);
-	coeff = 1 / kernel_(indexing, indexing) + noise^2 * eye(numel(indexing));
-	mean_ = mean_ + kernel_(:, indexing) * coeff * (points_train_target(count) - mean_(indexing));	
-	kernel_ = kernel_ - kernel_(:, indexing) * coeff * kernel_(indexing, :) + noise^2;
-	count = count + 1;
-end
+% for indexing = 1 : frequency : size(points, 1);
+% 	coeff = 1 / kernel_(indexing, indexing) + noise^2 * eye(numel(indexing));
+% 	mean_ = mean_ + kernel_(:, indexing) * coeff * (points_train_target(count) - mean_(indexing));	
+% 	kernel_ = kernel_ - kernel_(:, indexing) * coeff * kernel_(indexing, :) + noise^2;
+% 	count = count + 1;
+% end
 
 % % Implementation in one Stroke
 % indexing = 1 : frequency : size(points, 1);
@@ -73,8 +93,17 @@ end
 
 % R_post = mvnrnd( mean_, kernel_, 1);
 
-plot(points, mean_, 'g')
+% plot(points, mean_, 'g')
 % plot(points, R_post,'g');
-hold on;
-scatter(points_train, points_train_target)
+% hold on;
+% scatter(points_train, points_train_target)
 
+function reward = reward_dynamic(state, GRID)
+	if state == GRID
+		reward = 10;
+	elseif state == -GRID
+		reward = -10;	
+	else
+		reward = -1;
+	end
+end
---
+++ reward_dynamics.m
@@ -0,0 +1,12 @@
+
+
+
+reward_dynamic(5)
+
+function y = reward_dynamic(state)
+	if state == 5
+		y = 100;
+	else
+		y = -100;
+	end
+end
