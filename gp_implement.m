d1b16ca34b1c98656e35f65f92011321b4198b53
varun <varunsuryan9@gmail.com>
2017-09-28 10:07:21 -0400

    Q-learning for 1-d case

--- gp_implement.m
+++ gp_implement.m
@@ -1,15 +1,15 @@
 clear all;
 clc;
 close all;
-GRID = 5;
-epsilon = 0.05;
+GRID = 25;
+epsilon = 0.001;
 gamma_ =0.9;
 alpha_ = 1.0;
 % Create the state action space in 1-D
 states = [-GRID : 1 : GRID]';
 actions = [-1; 1]; 
 
-design_matrix = [repmat(states, numel(actions), 1) repmat(actions, numel(states), 1) zeros(numel(states) * numel(actions), 1)];
+design_matrix = [repmat(states, numel(actions), 1) repmat(actions, numel(states), 1) 100 * ones(numel(states) * numel(actions), 1)];
 
 
 % Append initial Q-values with state action space
@@ -45,36 +45,40 @@ mean_ = zeros(size(design_matrix, 1), 1);
 % points_train = points(1 : frequency : end);
 % points_train_target = sin(points_train) + 0.05 * randn(size(points_train));
 
+figure();
+for episode = 1 : 200
+Episode = episode
+curr_state = round(2 * GRID * (rand - 0.5)) ;
 
-curr_state = 2;
+	while curr_state ~= GRID	
+		if rand <= epsilon
+			action = datasample([-1 1], 1);
+		else
+			temp = design_matrix(find(design_matrix(:, 1) == curr_state), :);
+			[M, I] = max(temp(:, 3));
+			action = temp(I, 2);
+		end	
 
-while curr_state ~= GRID	
-	if rand <= epsilon
-		action = datasample([-1 1], 1);
-	else
-		temp = design_matrix(find(design_matrix(:, 1) == curr_state), :);
-		[M, I] = max(temp(:, 3));
-		action = temp(I, 2);
+		% next_state = (max(min(curr_state(1) + action(1), GRID), -GRID) , max(min(curr_state(2) + action(2), GRID), -GRID));
+		next_state = max(min(curr_state + action, GRID), -GRID);	
+
+		[~, indexing, ~] = intersect(design_matrix(:, 1 : end -1), [curr_state action], 'rows');	
+
+		observation = (1 - alpha_) * design_matrix(indexing, end) + alpha_ * reward_dynamic(next_state, GRID) + alpha_ * gamma_ * max(design_matrix(find(design_matrix(:, 1) == next_state), end));
+		design_matrix(indexing, end) = observation;	
+
+		% coeff = 1 / (kernel_(indexing, indexing) + noise^2 * eye(numel(indexing)));	
+
+		% mean_ = mean_ + kernel_(:, indexing) * coeff * (observation - mean_(indexing));	
+		% kernel_ = kernel_ - kernel_(:, indexing) * coeff * kernel_(indexing, :) + noise^2;
+		% design_matrix(:, end) = mean_ + 1.96 * diag(kernel_);
+		% y_pred, sigma = gp.predict(test, return_std = True)
+		curr_state = next_state;
+		scatter(curr_state, 0, 'filled');
+		axis([-GRID GRID -1 1]);
+		pause(0.001);
 	end
-
-	% next_state = (max(min(curr_state(1) + action(1), GRID), -GRID) , max(min(curr_state(2) + action(2), GRID), -GRID));
-	next_state = max(min(curr_state + action, GRID), -GRID);
-
-	[~, indexing, ~] = intersect(design_matrix(:, 1 : end -1), [curr_state action], 'rows');
-
-	observation = (1 - alpha_) * design_matrix(indexing, end) + alpha_ * reward_dynamic(next_state, GRID) + alpha_ * gamma_ * max(design_matrix(find(design_matrix(:, 1) == next_state), end));
-	
-	coeff = 1 / (kernel_(indexing, indexing) + noise^2 * eye(numel(indexing)));
-
-	mean_ = mean_ + kernel_(:, indexing) * coeff * (observation - mean_(indexing));	
-	kernel_ = kernel_ - kernel_(:, indexing) * coeff * kernel_(indexing, :) + noise^2;
-	design_matrix(:, end) = mean_ + 1.96 * diag(kernel_);
-	% y_pred, sigma = gp.predict(test, return_std = True)
-	curr_state = next_state
-	pause(1);
-	design_matrix
 end
-
 
 % Sequential Implementation
 % for indexing = 1 : frequency : size(points, 1);
