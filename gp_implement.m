478650f471303d171cf07974b64772811a869522
varun <varunsuryan9@gmail.com>
2017-09-28 12:42:11 -0400

    q-learning in two D

--- gp_implement.m
+++ gp_implement.m
@@ -1,16 +1,18 @@
 clear all;
 clc;
 close all;
-GRID = 25;
-epsilon = 0.001;
+GRID = 4;
+epsilon = 0.10;
 gamma_ =0.9;
-alpha_ = 1.0;
-% Create the state action space in 1-D
-states = [-GRID : 1 : GRID]';
-actions = [-1; 1]; 
+alpha_ = 0.8;
 
-design_matrix = [repmat(states, numel(actions), 1) repmat(actions, numel(states), 1) 100 * ones(numel(states) * numel(actions), 1)];
 
+% Create the state action space in 2-D
+[stateX, stateY] = meshgrid(-GRID: 1 :GRID, -GRID: 1 :GRID);
+actions = [0 1; 1 0; 0 -1; -1 0]; 
+state_action_space = [repmat([stateX(:) stateY(:)], size(actions, 1), 1) repmat(actions, numel(stateX), 1)];
+
+design_matrix = [state_action_space 100 * ones(numel(stateX) * size(actions, 1), 1)];
 
 % Append initial Q-values with state action space
 % design_matrix = [state_action_space zeros(size(state_action_space(:, 1)))];
@@ -46,67 +48,43 @@ mean_ = zeros(size(design_matrix, 1), 1);
 % points_train_target = sin(points_train) + 0.05 * randn(size(points_train));
 
 figure();
+
 for episode = 1 : 200
 Episode = episode
-curr_state = round(2 * GRID * (rand - 0.5)) ;
-
-	while curr_state ~= GRID	
+% curr_state = [round(2 * GRID * (rand - 0.5)) 2 * round(GRID * (rand - 0.5))]; 
+curr_state = [-GRID -GRID];
+	while ~isequal(curr_state, [GRID GRID])	
 		if rand <= epsilon
-			action = datasample([-1 1], 1);
+			action = actions(randsample(4, 1), :);
 		else
-			temp = design_matrix(find(design_matrix(:, 1) == curr_state), :);
-			[M, I] = max(temp(:, 3));
-			action = temp(I, 2);
+			temp = design_matrix(find(ismember(design_matrix(:, 1:2), curr_state, 'rows')), :);
+			I = datasample(find(temp(:, end) == max(temp(:, end))), 1);
+			action = temp(I, 3 : 4);
 		end	
 
-		% next_state = (max(min(curr_state(1) + action(1), GRID), -GRID) , max(min(curr_state(2) + action(2), GRID), -GRID));
-		next_state = max(min(curr_state + action, GRID), -GRID);	
+		next_state = [max(min(curr_state(1) + action(1), GRID), -GRID)  max(min(curr_state(2) + action(2), GRID), -GRID)];
 
 		[~, indexing, ~] = intersect(design_matrix(:, 1 : end -1), [curr_state action], 'rows');	
 
-		observation = (1 - alpha_) * design_matrix(indexing, end) + alpha_ * reward_dynamic(next_state, GRID) + alpha_ * gamma_ * max(design_matrix(find(design_matrix(:, 1) == next_state), end));
+		argmax = max(design_matrix(find(ismember(design_matrix(:, 1:2), next_state, 'rows')), :));
+		observation = (1 - alpha_) * design_matrix(indexing, end) + alpha_ * reward_dynamic(next_state, GRID) + alpha_ * gamma_ * argmax(end);
 		design_matrix(indexing, end) = observation;	
 
-		% coeff = 1 / (kernel_(indexing, indexing) + noise^2 * eye(numel(indexing)));	
+		scatter(curr_state(1), curr_state(2), 100, 'filled');
+		axis([-GRID GRID -GRID GRID]);
+		% grid on;
 
-		% mean_ = mean_ + kernel_(:, indexing) * coeff * (observation - mean_(indexing));	
-		% kernel_ = kernel_ - kernel_(:, indexing) * coeff * kernel_(indexing, :) + noise^2;
-		% design_matrix(:, end) = mean_ + 1.96 * diag(kernel_);
-		% y_pred, sigma = gp.predict(test, return_std = True)
 		curr_state = next_state;
-		scatter(curr_state, 0, 'filled');
-		axis([-GRID GRID -1 1]);
-		pause(0.001);
+		
+		pause(0.01);
 	end
 end
 
-% Sequential Implementation
-% for indexing = 1 : frequency : size(points, 1);
-% 	coeff = 1 / kernel_(indexing, indexing) + noise^2 * eye(numel(indexing));
-% 	mean_ = mean_ + kernel_(:, indexing) * coeff * (points_train_target(count) - mean_(indexing));	
-% 	kernel_ = kernel_ - kernel_(:, indexing) * coeff * kernel_(indexing, :) + noise^2;
-% 	count = count + 1;
-% end
-
-% % Implementation in one Stroke
-% indexing = 1 : frequency : size(points, 1);
-% coeff = pinv(kernel_(indexing, indexing) + noise^2 * eye(numel(indexing)));
-% mean_ = mean_ + kernel_(:, indexing) * coeff * (points_train_target - mean_(indexing));	
-% kernel_ = kernel_ - kernel_(:, indexing) * coeff * kernel_(indexing, :) + noise^2;
-
-
-% R_post = mvnrnd( mean_, kernel_, 1);
-
-% plot(points, mean_, 'g')
-% plot(points, R_post,'g');
-% hold on;
-% scatter(points_train, points_train_target)
-
 function reward = reward_dynamic(state, GRID)
-	if state == GRID
-		reward = 10;
-	elseif state == -GRID
-		reward = -10;	
+	if isequal(state, [GRID GRID])
+		reward = 100;
+	elseif isequal(state, [-GRID -GRID])
+		reward = -100;	
 	else
 		reward = -1;
 	end
