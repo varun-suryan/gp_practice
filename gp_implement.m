bf365ad9a4c040db75a9eb9e25efcd987e6b0082
varun <varunsuryan9@gmail.com>
2017-09-28 16:21:23 -0400

    efficient q learning done for 2-d

--- gp_implement.m
+++ gp_implement.m
@@ -1,11 +1,14 @@
 clear all;
 clc;
 close all;
-GRID = 4;
+GRID = 25;
 epsilon = 0.10;
 gamma_ =0.9;
-alpha_ = 0.8;
+alpha_ = 0.6;
 
+
+v = VideoWriter('q_learning');
+open(v);
 
 % Create the state action space in 2-D
 [stateX, stateY] = meshgrid(-GRID: 1 :GRID, -GRID: 1 :GRID);
@@ -20,14 +23,15 @@ design_matrix = [state_action_space 100 * ones(numel(stateX) * size(actions, 1),
 
 % surf(stateX, stateY, stateX.^2 + stateY.^2);
 
-l = 1/sqrt(2);
+% l = 1/sqrt(2);
 
-signal_std = 5;
-noise = 0.1;
+% signal_std = 5;
+% noise = 0.1;
 
-kernel_ = signal_std^2 * exp (- squareform(pdist(design_matrix(:, end - 1))).^2 / (2 * l^2) ) + noise^2 * eye(size(design_matrix,1));
+% kernel_ = signal_std^2 * exp (- squareform(pdist(design_matrix(:, end - 1))).^2 / (2 * l^2) ) + noise^2 * eye(size(design_matrix,1));
 
-mean_ = zeros(size(design_matrix, 1), 1);
+% mean_ = zeros(size(design_matrix, 1), 1);
+
 % R_prior = mvnrnd(mean_, kernel_, 1);
 
 
@@ -53,7 +57,13 @@ for episode = 1 : 200
 Episode = episode
 % curr_state = [round(2 * GRID * (rand - 0.5)) 2 * round(GRID * (rand - 0.5))]; 
 curr_state = [-GRID -GRID];
-	while ~isequal(curr_state, [GRID GRID])	
+% count = 0;
+	while ~isequal(curr_state, [GRID GRID]) 
+		% count = count + 1;
+		% if count > 200
+		% 	break;
+		% end
+
 		if rand <= epsilon
 			action = actions(randsample(4, 1), :);
 		else
@@ -67,11 +77,13 @@ curr_state = [-GRID -GRID];
 		[~, indexing, ~] = intersect(design_matrix(:, 1 : end -1), [curr_state action], 'rows');	
 
 		argmax = max(design_matrix(find(ismember(design_matrix(:, 1:2), next_state, 'rows')), :));
-		observation = (1 - alpha_) * design_matrix(indexing, end) + alpha_ * reward_dynamic(next_state, GRID) + alpha_ * gamma_ * argmax(end);
+		observation = (1 - alpha_) * design_matrix(indexing, end) + alpha_ * reward_dynamic(next_state, action, GRID) + alpha_ * gamma_ * argmax(end);
 		design_matrix(indexing, end) = observation;	
 
 		scatter(curr_state(1), curr_state(2), 100, 'filled');
 		axis([-GRID GRID -GRID GRID]);
+		frame = getframe(gcf);
+   		writeVideo(v,frame);
 		% grid on;
 
 		curr_state = next_state;
@@ -79,13 +91,12 @@ curr_state = [-GRID -GRID];
 		pause(0.01);
 	end
 end
+close(v);
 
-function reward = reward_dynamic(state, GRID)
+function reward = reward_dynamic(state, action, GRID)
 	if isequal(state, [GRID GRID])
 		reward = 100;
-	elseif isequal(state, [-GRID -GRID])
-		reward = -100;	
 	else
-		reward = -1;
+		reward = 10 * sum(action) + randn;
 	end
 end
